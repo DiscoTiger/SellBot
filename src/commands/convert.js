92d3226150b2c7a0e2ab6c0b3b017e8a912864b6
DicsoTiger <caden.n.mccauley@gmail.com>
2018-01-11 15:40:26 -0800

    spelling error

--- src/commands/convert.js
+++ src/commands/convert.js
@@ -8,7 +8,7 @@ class Convert extends Command {
 			description: 'Converts one cryptocurrency to another',
 			use: [
 				['<symbol>', true],
-				['<ammount>', true],
+				['<amount>', true],
 				['<symbol2> - defaults to USD', false]
 			],
 			example: '*convert BTC 1* - Convert 1 BTC to USD\n\t*convert BTC 1 ETH* - Convert 1 BTC to ETH',
@@ -23,16 +23,16 @@ class Convert extends Command {
 
 	run(msg, args) {
 		const symbol1 = args[0].toUpperCase();
-		const ammount = args[1];
+		const amount = args[1];
 		let symbol2;
 		if (args[2]) symbol2 = args[2].toUpperCase();
 		if (!this.client.tickers.has(symbol1)) return msg.reply(`Invalid cryptocurrency symbol: ${symbol1}`);
 		if (symbol2) {
 			if (!this.client.tickers.has(symbol2)) return msg.reply(`Invalid cryptocurrency symbol: ${symbol2}`);
-			let numOther = (ammount * this.client.tickers.get(symbol1).price_btc) / this.client.tickers.get(symbol2).price_btc; // eslint-disable-line max-len
+			let numOther = (amount * this.client.tickers.get(symbol1).price_btc) / this.client.tickers.get(symbol2).price_btc; // eslint-disable-line max-len
 			msg.channel.send(`${numOther} ${symbol2}`);
 		} else {
-			msg.channel.send(`$${this.client.tickers.get(symbol1).price_usd * ammount}`);
+			msg.channel.send(`$${this.client.tickers.get(symbol1).price_usd * amount}`);
 		}
 	}
 }
